c8018b75448b352eb50600670490b4513432cca8
Fatih Delice <delicex58@gmail.com>
2022-12-26 15:37:32 +0300

    First files added

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//MIDDLEWARE
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
